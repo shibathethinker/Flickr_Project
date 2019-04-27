b6c4468c93d9db35f6148309bb51040a52effb6c
shibathethinker <shibassmb@gmail.com>
2019-04-27 19:09:25 +1000

    new feature

--- angularApp/src/app/http-service/http-service.ts
+++ angularApp/src/app/http-service/http-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http,Response,Headers} from '@angular/http';
 import "rxjs/Rx";
 import { map } from "rxjs/operators";
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'  // <- ADD THIS
@@ -14,7 +15,8 @@ export class HttpService {
   public getPublicFeed()
 {
   return  this.http.
-  get('http://localhost:8100/flick/').pipe(map((response:Response) =>
+  //get('http://localhost:8100/flick/').pipe(map((response:Response) =>
+  get(environment.apiUrl).pipe(map((response:Response) =>
    (response.json()) )   
     );
 }
--- angularApp/src/environments/environment.prod.ts
+++ angularApp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'http://apiflickr1.australiaeast.azurecontainer.io:8100/flick/'
 };
